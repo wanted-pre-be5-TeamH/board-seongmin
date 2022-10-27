b2d570180edd5277e7fe59c74c2135b1637fefea
Gil Seongmin(Mark) <ksm65501@gmail.com>
2022-10-27 10:07:53 +0900

    feat:로그인 API 구현 (#3)

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,16 @@
-const { insertUser } = require("../services/userService");
+const { insertUser, updateLogin } = require("../services/userService");
 
 const postSignup = async (req, res) => {
   await insertUser(req.body);
   return res.status(200).json({ message: "USER_SIGNUP" });
 };
 
+const postLogin = async (req, res) => {
+  const accessToken = await updateLogin(req.body);
+  return res.status(200).json({ accessToken });
+};
+
 module.exports = {
   postSignup,
+  postLogin,
 };
